bb4181df6e3f113abd4d918427de74c994a993a9
Pepino <pepino.francois@gmail.com>
2018-09-21 20:07:32 +0200

    Amusons nous avec des Leds !

--- PIC_TRR2018.X/main.c
+++ PIC_TRR2018.X/main.c
@@ -20,6 +20,11 @@
 #include <stdint.h>
 #include "Timers.h"
 
+#define LED5 LATB5
+#define LED4 LATB4
+#define LED3 LATB3
+#define LED2 LATB2
+#define LED1 LATB1
 
 
 // Clock 64Mhz
@@ -65,6 +70,9 @@ void main(int argc, char** argv)
     uint16_t Val_Timer = 0;
     uint16_t Last_Val_Timer = 0;
     uint16_t val16;
+    
+    uint8_t Led = 0;
+    uint8_t Mode = 0;
   
     // penser à reséparer en fichier propre
     
@@ -83,20 +91,78 @@ void main(int argc, char** argv)
     
     // active sortie LED
     TRISCbits.TRISC5 = 0;
+    TRISB5 = 0;
+    TRISB4 = 0;
+    TRISB3 = 0;
+    TRISB2 = 0;
+    TRISB1 = 0;
+    
     
     while(1)
     {
-        
         TMR0H   =0x00;
         TMR0L   =0x00;
         Val_Timer = 0;
-        while (Val_Timer < 62500) {     //62.5kHz
+        while (Val_Timer < (62500/5)) {     //62.5kHz
             Val_Timer = TMR0L;
             val16 = TMR0H;
             val16 = val16 << 8;
             Val_Timer += val16;
             Last_Val_Timer = Val_Timer;
         }
+        if (Mode < 10) {
+            Led ++;
+            if (Mode & 1) {
+                LED1 = (Led != 1);  // s'alume si LED = 1
+                LED2 = (Led != 2);  // s'alume si LED = 1
+                LED3 = (Led != 3);  // s'alume si LED = 1
+                LED4 = (Led != 4);  // s'alume si LED = 1
+                LED5 = (Led != 5);  // s'alume si LED = 1
+            } else {
+                LED1 = (Led != 5);  // s'alume si LED = 1
+                LED2 = (Led != 4);  // s'alume si LED = 1
+                LED3 = (Led != 3);  // s'alume si LED = 1
+                LED4 = (Led != 2);  // s'alume si LED = 1
+                LED5 = (Led != 1);  // s'alume si LED = 1
+            }
+            if (Led == 5){
+                Mode ++;
+                Led = 0;
+            }
+        } else if (Mode < 20) {
+            if (Mode & 1) {
+                LED1 = 0;
+                LED2 = 1;
+                LED3 = 0;
+                LED4 = 1;
+                LED5 = 0;
+            } else {
+                LED1 = 1;
+                LED2 = 0;
+                LED3 = 1;
+                LED4 = 0;
+                LED5 = 1;
+            }
+            Mode++;
+        } else if (Mode < 30) {
+            if (Mode & 1) {
+                LED1 = 0;
+                LED2 = 0;
+                LED3 = 0;
+                LED4 = 0;
+                LED5 = 0;
+            } else {
+                LED1 = 1;
+                LED2 = 1;
+                LED3 = 1;
+                LED4 = 1;
+                LED5 = 1;
+            }
+            Mode++;
+        } else {
+            Mode = 0;
+        }
+        
         // joue avec la sortie LED
         LATCbits.LATC5 = 1 - LATCbits.LATC5;
         // Lecture pin in Raspi IO
